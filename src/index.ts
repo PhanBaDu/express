5cb1e3ec3481c5e1236976fbd0b4dadbe847d430
Phan Ba Du <duphan03@gmail.com>
2025-07-01 23:12:55 +0700

    feat(mongoose): connect database

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // [IMPORTS]
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './configs/db.connect'
 // [IMPORT ROUTES]
 import testRoute from './routes/test.route'
 
@@ -16,5 +17,6 @@ app.use('/api/test', testRoute)
 // [PORT]
 const port = process.env.PORT || 3001
 app.listen(port, () => {
+  connectDB()
   console.log(`Server is running on port ${port}`)
 })
